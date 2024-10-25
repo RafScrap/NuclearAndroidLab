6c50cdf4be6a15a1eef870cf2f69a265871dbfe8
Alexandra Bobrovskaya <black.sakyra@gmail.com>
2024-10-25 19:57:40 +0300

    hw2 part1

--- app/src/main/java/com/example/nuclearandroidlab/ActivityB.kt
+++ app/src/main/java/com/example/nuclearandroidlab/ActivityB.kt
@@ -6,28 +6,28 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.nuclearandroidlab.ui.theme.NuclearAndroidLabTheme
 
-class ActivityA : ComponentActivity() {
+class ActivityB : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_a)
+        setContentView(R.layout.activity_b)
 
         //enableEdgeToEdge()
-        val buttonOpenActivityB = findViewById<Button>(R.id.button_activity_a_open_activity_b)
-        buttonOpenActivityB.setOnClickListener {
-            val intent = Intent(this, ActivityB::class.java)
-                .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        val buttonOpenActivityC = findViewById<Button>(R.id.button_activity_b_open_activity_c)
+        buttonOpenActivityC.setOnClickListener {
+            val intent = Intent(this, ActivityC::class.java)
+                //.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT)
             startActivity(intent)
-        }
-        val buttonOpenFragmentB = findViewById<Button>(R.id.button_activity_a_open_fragment_b)
-        buttonOpenFragmentB.setOnClickListener {
         }
     }
 }
---
+++ app/src/main/java/com/example/nuclearandroidlab/ActivityC.kt
@@ -0,0 +1,25 @@
+package com.example.nuclearandroidlab
+
+import android.app.ActivityManager
+import android.app.ActivityManager.RunningTaskInfo
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.activity.ComponentActivity
+
+
+class ActivityC : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_c)
+
+        val buttonOpenActivityA = findViewById<Button>(R.id.button_activity_c_open_activity_a)
+        buttonOpenActivityA.setOnClickListener {
+            val intent = Intent(this, ActivityA::class.java)
+                .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent);
+        }
+    }
+}
