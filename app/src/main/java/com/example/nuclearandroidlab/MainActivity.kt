70fda1631e600d479293e94b7f95a8b1131b9e52
Alexandra Bobrovskaya <black.sakyra@gmail.com>
2024-11-03 18:36:39 +0300

    AppCompatActivity not work

--- app/src/main/java/com/example/nuclearandroidlab/MainActivity.kt
+++ app/src/main/java/com/example/nuclearandroidlab/MainActivity.kt
@@ -2,8 +2,9 @@ package com.example.nuclearandroidlab
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()
