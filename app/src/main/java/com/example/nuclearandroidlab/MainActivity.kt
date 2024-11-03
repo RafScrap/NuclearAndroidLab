293c9a29fdba2d054024dd5bf4b92c4d8896e2fb
Alexandra Bobrovskaya <black.sakyra@gmail.com>
2024-11-03 22:12:03 +0300

    hw3 is done

--- app/src/main/java/com/example/nuclearandroidlab/MainActivity.kt
+++ app/src/main/java/com/example/nuclearandroidlab/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.example.nuclearandroidlab
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-/*import android.app.NotificationChannel
+import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
 import android.os.Build
@@ -15,13 +16,13 @@ import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkManager
 import androidx.work.Worker
 import androidx.work.WorkerParameters
-import java.util.concurrent.TimeUnit*/
+import java.util.concurrent.TimeUnit
 
 
-/*class EverydayWorker(context: Context, workerParams: WorkerParameters) :
+class EverydayWorker(context: Context, workerParams: WorkerParameters) :
     Worker(context, workerParams) {
     override fun doWork(): Result {
-        Log.d("show", "show2")
+        Log.d("show", "show1")
         try {
             notifactionShow("title","desc");
         } catch (ex: Exception) {
@@ -31,39 +32,43 @@ import java.util.concurrent.TimeUnit*/
     }
 
     fun notifactionShow(title: String?, desc: String?) {
-        Log.d("show", "show1")
         val notificationManager =
             applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channelId = "channelId"
             val channel = NotificationChannel(
-                "@string/channel_id",
-                "@string/channel_id",
+                channelId,
+                channelId,
                 NotificationManager.IMPORTANCE_DEFAULT
             )
             notificationManager.createNotificationChannel(channel)
             val builder: NotificationCompat.Builder =
-                NotificationCompat.Builder(applicationContext, "@string/channel_id")
+                NotificationCompat.Builder(applicationContext, channelId)
                     .setContentTitle(title)
                     .setContentText(desc)
                     .setSmallIcon(R.mipmap.ic_launcher)
 
             notificationManager.notify(1, builder.build())
+            Log.d("hw3", "addNotification")
         }
     }
-}*/
+}
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()
         setContentView(R.layout.layout_main_activity)
-        /*val workRequest : PeriodicWorkRequest =
+
+        val channelId = getString(R.string.channel_id)
+        val workRequest: PeriodicWorkRequest =
             PeriodicWorkRequestBuilder<EverydayWorker>(
-                3, TimeUnit.MINUTES,
-                1, TimeUnit.MINUTES
-            ).build()
-        Log.d("show", "show0")
-        WorkManager.getInstance(application.applicationContext).enqueueUniquePeriodicWork("PWR",
-            ExistingPeriodicWorkPolicy.KEEP, workRequest)*/
+            1, TimeUnit.DAYS,
+            5, TimeUnit.MINUTES
+        ).build()
+        WorkManager.getInstance(application.applicationContext).enqueueUniquePeriodicWork(
+            channelId, ExistingPeriodicWorkPolicy.KEEP, workRequest
+        )
+        Log.d("hw3", "onCreate")
     }
 }
